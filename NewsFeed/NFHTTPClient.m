599082322cd7e84bfda34a265a0714d7828dc188
Hemang <hemangshah.in@gmail.com>
2016-03-19 14:08:54 +0530

    Make API call
    Subclasses AFHTTPRequestOperationManager to handle get request in app.
    Successfully got response from the API.

---
+++ NewsFeed/NFHTTPClient.m
@@ -0,0 +1,36 @@
+//
+//  NFHTTPClient.m
+//  NewsFeed
+//
+//  Created by Hemang Shah on 19/03/16.
+//  Copyright (c) 2016 Hemang Shah. All rights reserved.
+//
+
+#import "NFHTTPClient.h"
+
+static NSString * const kBaseUrl = @"https://api.myjson.com/bins/vfpj";
+
+@interface NFHTTPClient()
+@end
+
+@implementation NFHTTPClient
+static NFHTTPClient *sharedInstance = nil;
++ (NFHTTPClient *) sharedInstance {
+        static dispatch_once_t onceToken;
+        dispatch_once(&onceToken, ^{
+            if(!sharedInstance) {
+                sharedInstance = [[NFHTTPClient alloc] init];
+            }
+        });
+    return sharedInstance;
+}
+
+- (instancetype) init {
+    self = [super initWithBaseURL:[NSURL URLWithString:kBaseUrl]];
+    if(self) {
+        self.responseSerializer = [AFJSONResponseSerializer serializer];
+        self.responseSerializer.acceptableContentTypes = [self.responseSerializer.acceptableContentTypes setByAddingObject:@"text/html"];
+    }
+    return self;
+}
+@end
--- NewsFeed/NewsListViewController.m
+++ NewsFeed/NewsListViewController.m
@@ -8,15 +8,25 @@
 
 #import "NewsListViewController.h"
 
-@interface NewsListViewController ()
+#import <UIImageView+AFNetworking.h>
 
+#import "NFHTTPClient.h"
+
+#import "NewsItem.h"
+
+static NSString * const kNewsItemResponse = @"items";
+
+@interface NewsListViewController () {
+    NSMutableArray *arrayNewsItems;
+}
 @end
 
 @implementation NewsListViewController
-
+#pragma mark - View Life Cycle
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self requestNewsFeeds];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,14 +34,39 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+#pragma mark - Requests
+- (void) requestNewsFeeds {
+    //We've subclassed AFHTTPRequestOperationManager (AFNetworking) to handle following GET request.
+    //The reason for subclassing is to have a singleton object for requests.
+    [[NFHTTPClient sharedInstance] GET:@"" parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        //Yeaah! we got some response.
+        [self parseResult:responseObject];
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        //Ummm. There's some error.
+        NSLog(@"%@", error);
+    }];
 }
-*/
 
+#pragma mark - Parse Result
+- (void) parseResult:(id)responseObject {
+    if(responseObject) {
+        //Update arrayNewsItems before adding new NewsItem.
+        if(!arrayNewsItems) {
+            arrayNewsItems = [NSMutableArray array];
+        } else {
+            [arrayNewsItems removeAllObjects];
+        }
+        //Fetch array of items from the response
+        NSArray *items = [NSArray arrayWithArray:[responseObject objectForKey:kNewsItemResponse]];
+        if(items) {
+            //Loop through each item and get a dictionary, later it will become NewsItem.
+            for(NSDictionary *newsDictionary in items) {
+                //Convert dictionary to NewsItem object.
+                //Add into arrayNewsItems.
+                [arrayNewsItems addObject:[[NewsItem alloc] initWithNewsDictionary:newsDictionary]];
+            }
+        }
+        NSLog(@"%@", arrayNewsItems);
+    }
+}
 @end
